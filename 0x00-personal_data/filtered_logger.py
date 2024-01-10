25d30a5db7f0a54f73e8242b825cc98824fc9d7f
Esubalew197 <esubalewmillion@gmail.com>
2024-01-10 23:39:31 +0300

    Task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 '''personal data protection module
 '''
+import logging
 import re
 from typing import List
 
@@ -18,3 +19,23 @@ def filter_datum(
     '''
     extract, replace = (patterns["extract"], patterns["replace"])
     return re.sub(extract(fields, separator), replace(redaction), message)
+
+
+class RedactingFormatter(logging.Formatter):
+    '''Redacting Formatter class
+    '''
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        '''format the log record
+        '''
+        msg = super(RedactingFormatter, self).format(record)
+        txt = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
+        return txt
