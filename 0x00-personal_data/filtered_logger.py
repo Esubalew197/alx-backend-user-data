766cf11fc30ab68464d6bf2c903f8ae0c7d4d056
Esubalew197 <esubalewmillion@gmail.com>
2024-01-10 23:26:29 +0300

    Task 0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''personal data protection module
+'''
+import re
+from typing import List
+
+
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+
+
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str,
+) -> str:
+    '''obfuscate log messages
+    '''
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)
