606367e247195a12a5eee6b57701bb18940ad05f
Esubalew197 <esubalewmillion@gmail.com>
2024-01-16 20:07:01 +0300

    Task 7. Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,8 +2,24 @@
 '''Basic authentication
 '''
 from .auth import Auth
+import re
+from typing import Tuple, TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
     '''Basic Auth class
     '''
+
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
+        '''Extracts the Base64 part of the Authorization header
+        for a Basic Authentication.
+        '''
+        if type(authorization_header) == str:
+            pattern = r'Basic (?P<token>.+)'
+            field_match = re.fullmatch(pattern, authorization_header.strip())
+            if field_match is not None:
+                return field_match.group('token')
+        return None
