7a6055600650ca8d3e3fdbd78c552abc50d3b1e8
Esubalew197 <esubalewmillion@gmail.com>
2024-01-16 19:06:04 +0300

    Update Task 3

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,10 +11,20 @@ class Auth:
     '''
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        return False
+        '''Ensures authentication is required
+        '''
+        if path is not None and excluded_paths is not None:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
+        '''includes authorization header to http req
+        '''
+        if request is not None:
+            return None
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
+        '''Retrieves the current users
+        '''
         return None
